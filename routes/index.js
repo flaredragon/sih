1a953b919574c123e323a99f1d5326fd9c5d24e4
Akashdeep Ramnaney <akashdeepramnaney@gmail.com>
2018-03-30 20:55:36 +0530

    Corrected IP

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dashboard', { count: 'Express',time : new Date() });
+  res.render('dashboard', { count: '4',time : new Date() });
 });
 
 module.exports = router;
